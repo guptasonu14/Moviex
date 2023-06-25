ef80aef28b67e2fe29278407683798829480b689
guptasonu14 <sguptabudha@gmail.com>
2023-06-25 12:01:15 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import {fetchDataFromApi} from "./utils/api"
-import { useSelector, useDispatch } from 'react-redux'
-import {getApiConfiguration} from "./store/homeSlice";
+import {fetchDataFromApi} from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux';
+import {getApiConfiguration ,getGenres} from "./store/homeSlice";
 import Header from "./componenets/header/Header";
 import Footer from "./componenets/footer/Footer";
 import Home from './pages/home/Home';
@@ -18,32 +18,57 @@ function App() {
  console.log(url);
 
   useEffect(()=>{
-    apiTesting();
+    fetchApiConfig();
+    genresCall();
 
   },[]);
   
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular').then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res))
-    })
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
   };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+};
+
   
 
-  return(
-    <BrowserRouter>
-        <Header />  
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/:mediaType/:id" element={<Details />} />
-                <Route path="/search/:query" element={<SearchResult />} />
-                <Route path="/explore/:mediaType" element={<Explore />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          <Footer /> 
-        </BrowserRouter>
-    
-  );
+return (
+  <BrowserRouter>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+  </BrowserRouter>
+);
    
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import "./style.scss";
-import HeroBanner from './heroBanner/HeroBanner';
+import React from "react";
 
-function Home() {
-  return (
-    <div className='homePage'>
-      <HeroBanner />
-    </div>
-  );
+import "./style.scss";
+
+import HeroBanner from "./heroBanner/HeroBanner";
+
+
+const Home = () => {
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            
+        </div>
+    );
 };
 
 export default Home;
