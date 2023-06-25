0a25834a14b8e7793992b1289da573755becabf2
guptasonu14 <sguptabudha@gmail.com>
2023-06-25 17:34:06 +0530

    add trending section

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
+
 import ContentWrapper from '../../../componenets/contentWrapper/ContentWrapper';
 
 import SwitchTabs from '../../../componenets/switchTabs/SwitchTabs';
 
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-    const onTabChange = (tav) => {};
+    const [endpoint, setEndpoint] = useState("day");
 
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
-  return (
-   <div className="carouseTitle">Trending
-   <ContentWrapper>
-    <span className="carouseTitle"></span>
-    <SwitchTabs  data={["Day" ,"Week"]} onTabChange={onTabChange} />
-   </ContentWrapper>
-   </div>
-  );
+    const onTabChange = (tab) => {
+        setEndpoint(tab === "Day" ? "day" : "week");
+    };
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Trending</span>
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+            </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
+        </div>
+    );
 };
 
 export default Trending;
