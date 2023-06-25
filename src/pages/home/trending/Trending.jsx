9fbe0d0eed6c67b4de6bb7f474dc2bca4d541737
guptasonu14 <sguptabudha@gmail.com>
2023-06-25 17:16:39 +0530

    add switchtab

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import ContentWrapper from '../../../componenets/contentWrapper/ContentWrapper';
+
+import SwitchTabs from '../../../componenets/switchTabs/SwitchTabs';
+
 
 const Trending = () => {
+    const onTabChange = (tav) => {};
+
+
   return (
-    <div>Trending</div>
-  )
-}
+   <div className="carouseTitle">Trending
+   <ContentWrapper>
+    <span className="carouseTitle"></span>
+    <SwitchTabs  data={["Day" ,"Week"]} onTabChange={onTabChange} />
+   </ContentWrapper>
+   </div>
+  );
+};
 
 export default Trending;
